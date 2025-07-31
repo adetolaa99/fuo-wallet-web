00c7b45fc80a01965e6d95137fd862caafd2e6af
Hassan Nofisat Adetola <hassannofisat017@gmail.com>
2025-07-31 15:48:11 +0100

    chore: add backend url

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
-const ENV = "development"; //"production"
+const ENV = "production"; //"development"
 
 const BASE_URLS = {
   development: "http://localhost:8080",
-  production: "",
+  production: "https://fuo-wallet-backend.onrender.com",
 };
 
 export const API_URL = `${BASE_URLS[ENV]}/api`;
